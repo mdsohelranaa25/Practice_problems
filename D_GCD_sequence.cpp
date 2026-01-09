fed6337816b343fc2b824836c72732c5ca95e9d2
mdsohelraa25 <mdsohelranaa25@gmail.com>
2026-01-10 02:19:21 +0600

    10/1/26 cf

--- D_GCD_sequence.cpp
+++ D_GCD_sequence.cpp
@@ -1,106 +1,84 @@
-		/*
-			//! In The Name Of Allah
-				
-			
-				
-				
-		*/                                                                                                                                                  
-  #include<bits/stdc++.h>
-  using namespace std;
-  #define tt int t;cin>>t;while(t--)
-  #define itr(i,a) for(int i=0;i<a;i++)
-#define ittr(i,bg,a) for(int i=bg;i<a;i++)
-  #define yes cout<<"YES"<<endl
-  #define no cout<<"NO"<<endl
-  #define ll long long
-  #define endd cout<<endl
-  #define vc(v, n) vector<ll> v(n)  
-  #define srt(v) sort(v.begin(), v.end())
+/*
+  //! In The Name Of Allah
+
+
+
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define tt  \
+  int t;    \
+  cin >> t; \
+  while (t--)
+#define itr(i, a) for (int i = 0; i < a; i++)
+#define ittr(i, bg, a) for (int i = bg; i < a; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define ll long long
+#define endd cout << endl
+#define vc(v, n) vector<ll> v(n)
+#define srt(v) sort(v.begin(), v.end())
 #define rsrt(v) sort(v.rbegin(), v.rend())
-#define pb(a,x) a.push_back(x)
+#define pb(a, x) a.push_back(x)
 #define MOD 1000000007
-  #define f first
-  #define sc second
+#define f first
+#define sc second
 #define endl '\n'
-const int N=1e6+1;
-vector<bool>isprime(N,false);
-vector<ll>primes;
-ll cnt_div[N]={0};
-vector<ll>divisor[N];
-void divis(){
-for(int i=1;i<=N;i++){
-for(int j=i;j<=N;j+=i){
-cnt_div[j]++;
-divisor[j].push_back(i);
-//if(i/j!=i) divisor[j].push_back(i/j);
-}
-}
+const int N = 1e6 + 1;
+vector<bool> isprime(N, false);
+vector<ll> primes;
 
-}
-void sieve(){
-isprime[1]=false;
-isprime[2]=true;
-pb(primes,2);
-for(int i=3;i*i<=N;i+=2) isprime[i]=true;
-for(int i=3;i<=N;i+=2){
-if(isprime[i]){
-pb(primes,i);
-for(int j=i*i;j<=N;j+=i){
-isprime[j]=false;
-}
-}
-}
-}
-
-  int main(){
-    ios::sync_with_stdio(false);
-      cin.tie(nullptr);
-    // sieve();
-   // divis();
-  tt{
- 
-ll n;
-cin>>n;
-vc(v,n);
-itr(i,n )cin>>v[i];
-ll gcd=__gcd(v[0],v[1]);
-bool f=0;
-bool ff=0;
-cerr<<gcd<<' ';
-for(int i=2;i<n;i++){
-    if(__gcd(gcd,v[i])<gcd){
-        if(f) {
-            ff=1;
-            break;
-        }
-        else{
-            f=1;
-           // continue;
-        }
-        
-    }
-    else gcd=__gcd(gcd,v[i]);
-    cerr<<gcd<<' ';
-}
-if(!ff) yes;
-else{
-    gcd=__gcd(v[1],v[2]);
-    cerr<<endl<<gcd<<' ';
-    f=0;
-    for(int i=3;i<n;i++){
-        if(__gcd(gcd,v[i])<gcd) {
-            f=1;
-            break;
-        }
-        else gcd=__gcd(gcd,v[i]);
-        cerr<<gcd<<' ';
-        
-    }
-    if(f) no;
-    else yes;
-
-}
+ bool isgood(vector<ll>&b){
+  ll n=b.size();
+  ll g=__gcd(b[0],b[1]);
+  for(int i=1;i<n-1;i++){
+    ll curg=__gcd(b[i],b[i+1]);
+    if(g>curg) return false;
+    else g=curg;
   }
-  }
-  
+  return true;
 
+ }
+void solve()
+{
+
+  ll n;
+  cin>>n;
+  vc(v,n);
+  itr(i,n) cin>>v[i];
+  ll todel=-1;
+  ll g=-1;
+  itr(i,n-1){
+    ll curg=__gcd(v[i],v[i+1]);
+    if(curg<g){
+      todel=i;
+      break;
+    }
+    else g=curg;
+
+  }
+  if(todel==-1) {
+    yes;
+    return;
+  }
+  vector<ll>a=v,b=v,c=v;
+  if(todel>0) a.erase(a.begin()+todel-1);
+  b.erase(b.begin()+todel);
+  if(todel<n-1) c.erase(c.begin()+todel+1);
+  if(isgood(a)||isgood(b)||isgood(c)) yes;
+  else no;
+
+
+}
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  // sieve();
+  // divis();
+  tt
+  {
+    solve();
+  }
+}
